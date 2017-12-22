0ef101253634ed0d1e5323d451d60a05d001b61f
davidmwang <dmwang@berkeley.edu>
2017-12-22 12:07:47 -0800

    Moved simulation code to separate folder and added example usage file

---
+++ simulation/example.py
@@ -0,0 +1,11 @@
+""" Example usage of simulation. """
+
+import simulation
+
+def main():
+    # Create and animate an example vineyard.
+    vy = simulation.Vineyard()
+    vy.animate()
+
+if __name__ == "__main__":
+    main()
