ad5d3eb4858e87a47e89561baa4adab04b46eef9
William Song <wsong@Appa.local>
2018-01-12 00:28:12 -0800

    Add PID control file

--- simulation/pid_loop_visualization.py
+++ simulation/pid_loop_visualization.py
@@ -46,6 +46,7 @@ im_resized.save(IMG_FILENAME.format(10), "PNG")
 # Intialize predictor.
 predictor = predictions.Predictor("/home/wsong/saved_models/whole_image/noise_0_training_1000.ckpt", tf.Session())
 
+total_irrigation_used = 0.0
 # Apply feedback controller for 20 more timesteps.
 for j in range(11, 31):
     # Update irrigation rate using feedback.
@@ -56,6 +57,9 @@ for j in range(11, 31):
 
     # Prevent irrigation rate from becoming negative.
     vy.irrigation_rate = vy.irrigation_rate.clip(min=0.0)
+
+    # Log total amount of irrigation used.
+    total_irrigation_used += np.sum(vy.irrigation_rate)
 
     # Run simulation for one timestep.
     vy.update(0)
@@ -68,7 +72,11 @@ for j in range(11, 31):
     im_resized = im.resize(size, Image.ANTIALIAS)
     im_resized.save(IMG_FILENAME.format(j), "PNG")
 
+
 print("Visualization Complete")
 print("Drain Rate:", RATE, "Standard Dev:", STD_DEV)
 print("File location:", IMG_FILENAME)
-
+print("AVERAGE IRRIGATION PER PLANT PER TIMESTEP:", total_irrigation_used / 200.0 / 10.0)
+num_leaves = sum([len(p.leaf_positions) for p in vy.vines])
+print("Avg Number of Leaves per plant:", num_leaves / 200.0)
+print("Irrigation per Leaf:", total_irrigation_used / num_leaves)
