d8f19f69d86fb52b0eef539c30052eb7b61b3c9d
William Song <wsong@Appa.local>
2018-01-12 17:09:18 -0800

    change graph title

--- simulation/pid_loop_visualization.py
+++ simulation/pid_loop_visualization.py
@@ -210,7 +210,7 @@ def one_prediction_irrigation():
     print("Irrigation per Leaf:", avg_irr_per_leaf)
 
     # plot error chart for PID tuning
-    title = "Avg leaves per plant: {0:.3f}, avg irrigation per plant per time step: {1:.3f}, irrigation per leaf: {2:.3f}".format(avg_leaves, avg_irrigation, avg_irr_per_leaf)
+    title = "Avg leaves/plant: {0:.3f}, avg irrigation/plant/time step: {1:.3f}, irrigation/leaf: {2:.3f}".format(avg_leaves, avg_irrigation, avg_irr_per_leaf)
     t = np.arange(11, 31)
     error_plot = plt.figure()
     ax = error_plot.add_subplot(111)
