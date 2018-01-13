43e229c2c027fa351b2b0aaba8d72b204fa3fc0a
William Song <wsong@Appa.local>
2018-01-12 16:06:29 -0800

    add plot for non-pid visualization

--- simulation/control_loop_visualization.py
+++ simulation/control_loop_visualization.py
@@ -46,16 +46,26 @@ im_resized.save(IMG_FILENAME.format(10), "PNG")
 # Intialize predictor.
 predictor = predictions.Predictor("/home/wsong/saved_models/whole_image/noise_0_training_1000.ckpt", tf.Session())
 
+total_irrigation_used = 0.0
+avg_errors = []
 # Apply feedback controller for 20 more timesteps.
 for j in range(11, 31):
+
+    error = predictor.predictions(IMG_FILENAME.format(j - 1)) - 0.25
     # Update irrigation rate using feedback.
-    vy.irrigation_rate += predictor.predictions(IMG_FILENAME.format(j - 1)) - 0.25
+    vy.irrigation_rate += error
+
+    # term to plot later
+    avg_errors.append(sum(error) / 200.0)
 
     # add noise to irrigation system output
     vy.irrigation_rate += np.random.normal(scale=STD_DEV, size=vy.irrigation_rate.shape)
 
     # Prevent irrigation rate from becoming negative.
     vy.irrigation_rate = vy.irrigation_rate.clip(min=0.0)
+
+    # Log total amount of irrigation used.
+    total_irrigation_used += np.sum(vy.irrigation_rate)
 
     # Run simulation for one timestep.
     vy.update(0)
@@ -71,4 +81,19 @@ for j in range(11, 31):
 print("Visualization Complete")
 print("Drain Rate:", RATE, "Standard Dev:", STD_DEV)
 print("File location:", IMG_FILENAME)
+avg_irrigation = total_irrigation_used / 200.0 / 20.0
+num_leaves = sum([len(p.leaf_positions) for p in vy.vines])
+avg_leaves = num_leaves / 200.0
+avg_irr_per_leaf = total_irrigation_used / num_leaves
+print("AVERAGE IRRIGATION PER PLANT PER TIMESTEP:", avg_irrigation)
 
+print("Avg Number of Leaves per plant:", avg_leaves)
+print("Irrigation per Leaf:", avg_irr_per_leaf)
+
+title = "Avg leaves per plant: {0:.3f}, avg irrigation per plant per time step: {1:.3f}, irrigation per leaf: {2:.3f}".format(avg_leaves, avg_irrigation, avg_irr_per_leaf)
+t = np.arange(11, 31)
+error_plot = plt.figure()
+ax = error_plot.add_subplot(111)
+ax.scatter(t, avg_errors)
+ax.set_title(title)
+error_plot.savefig(DIRECTORY + "error-plot")
